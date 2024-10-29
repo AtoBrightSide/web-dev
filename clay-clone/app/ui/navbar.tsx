276b1606847b745f4a544abc52d94df00626dbf1
Abel Mesele <meseleabel99@gmail.com>
2024-10-29 08:55:59 +0300

    Clean up Navbar

--- clay-clone/app/ui/navbar.tsx
+++ clay-clone/app/ui/navbar.tsx
@@ -147,7 +147,7 @@ export const Navigation = () => {
                 </AccordionItem>
             </Accordion>
 
-            <NavigationMenu className="hidden md:block">    
+            <NavigationMenu className="hidden md:block">
                 <NavigationMenuList>
                     <NavigationMenuItem>
                         <NavigationMenuTrigger>Products</NavigationMenuTrigger>
@@ -181,18 +181,14 @@ export const Navigation = () => {
                     <NavigationMenuItem>
                         <NavigationMenuTrigger>Solutions</NavigationMenuTrigger>
                         <NavigationMenuContent>
-                            <div className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2     lg:w-[600px] ">
+                            <div className="">
 
                             </div>
                         </NavigationMenuContent>
                     </NavigationMenuItem>
 
                     <NavigationMenuItem>
-                        <Link href="/docs" legacyBehavior passHref>
-                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                                Enterprise
-                            </NavigationMenuLink>
-                        </Link>
+                        <NavigationMenuTrigger>Enterprise</NavigationMenuTrigger>
                     </NavigationMenuItem>
 
                 </NavigationMenuList>
@@ -205,29 +201,3 @@ export const Navigation = () => {
         </nav>
     )
 }
-
-const ListItem = forwardRef<
-    React.ElementRef<"a">,
-    React.ComponentPropsWithoutRef<"a">
->(({ className, title, children, ...props }, ref) => {
-    return (
-        <li>
-            <NavigationMenuLink asChild>
-                <a
-                    ref={ref}
-                    className={cn(
-                        "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-                        className
-                    )}
-                    {...props}
-                >
-                    <div className="text-sm font-medium leading-none">{title}</div>
-                    <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-                        {children}
-                    </p>
-                </a>
-            </NavigationMenuLink>
-        </li>
-    )
-})
-ListItem.displayName = "ListItem"
